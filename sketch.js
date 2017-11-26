6e3d21ba486f0fa8c28cea9da966bbbddcf05322
Jason Madden <jason.madden8@mail.dcu.ie>
2017-11-26 02:22:02 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,90 +1,76 @@
-var player = new Player("black");
-var player_white = new Player("white");
-var board = new Board();
-board.buildBoard();
-var tmpX;
-var tmpY;
-
-function getScore() {
-	return player.score
+body {
+	background-image: url("woodenbackground.jpg");
+	position: relative;
 }
-
-function setup() {
-	createCanvas(900, 900); //create the canvas
-	console.log(board.board);
-	player.placeStone(190, 140, board);
-	player.placeStone(140, 190, board);
-	player.placeStone(190, 240, board);
-	player_white.placeStone(190, 190, board);
-}
-
-function checkMousePosition() {
-	tmpX = mouseX; //freeze the mouseX and mouseY at the particular frame 
-	tmpY = mouseY;
-	fill(0) ? player.color == "black" : fill(255);
-	for (var i = 0; i < board.size; i++) {
-		for (var j = 0; j < board.size; j++) {
-			if (tmpX > 0 && tmpY > 0 && tmpX < board.size * 50 + 80 && tmpY < board.size * 50 + 80) {
-				//if the mouse is in range of a tile on the board
-				if (tmpX > (board.board[i][j].x - 25) && tmpX < (board.board[i][j].x + 25) && tmpY > (board.board[i][j].y - 25) && tmpY < (board.board[i][j].y + 25)) {
-					tmpX = board.board[i][j].x;
-					tmpY = board.board[i][j].y;
-					ellipse(tmpX, tmpY, 45, 45);
-				}
-				//if the mouse is on a tile just out of range of the board list
-				//left side of the board testing constraints
-				else if (tmpX > (board.size * 50 + 15) && tmpX < (board.size * 50 + 65) && tmpY > (board.board[i][j].y - 25) && tmpY < (board.board[i][j].y + 25)) {
-					tmpX = board.size * 50 + 40;
-					tmpY = board.board[i][j].y;
-					ellipse(tmpX, tmpY, 45, 45);
-				}
-				//same again but bottom constraint
-				else if (tmpY > (board.size * 50 - 15) && tmpY < (board.size * 50 + 65) && tmpX > (board.board[i][j].x - 25) && tmpX < (board.board[i][j].x + 25)) {
-					tmpX = board.board[i][j].x;
-					tmpY = board.size * 50 + 40;
-					ellipse(tmpX, tmpY, 45, 45);
-				}
-				//ridiculous bottom right case 
-				else if (tmpX > (board.size * 50 + 15) && tmpX < (board.size * 50 + 65) && tmpY > (board.size * 50 - 15) && tmpY < (board.size * 50 + 65)) {
-					tmpX = board.size * 50 + 40;
-					tmpY = board.size * 50 + 40;
-					ellipse(tmpX, tmpY, 45, 45);
-				}
-				
-			}
+h1 {
+    color: #DBB36A;
+	text-align: center;
+	font-size: 5em;
+	font-family:courier;
 	
-				 
-		}
-	}
+}
+p {
+    color: white;
+    margin-left:1em;
 }
 
-function mousePressed() {
-	//fix a bug where even if the mouse was placed out of range stones could be placed off the board
-	//checks if it is the players turn 
-	if (tmpX >= 40 && tmpY >= 40 && player.currentTurn && tmpX <= board.size * 50 + 40 && tmpY <= board.size * 50 + 40) {  //fix this alert thing
-		var moveTaken = player.placeStone(tmpX, tmpY, board);
-		//SEND COORDINATES OF THE NEW STONE TO THE SERVER HERE
-		console.log(moveTaken);
-		if (moveTaken) {
-			//player.currentTurn = false;
-			capture(board, player.color, board.stones);
-		}
-	}
-	else if (mouseX >= 0 && mouseY >= 0 && mouseX <= (board.size * 50 + 80) && mouseY <= (board.size * 50 + 80) && !player.currentTurn) {
-		alert("It is your opponents turn please wait");
-	}
-	//since with the check method tmpX and tmpY is the coordinates of the stone you can use this
-	//to place the stone in the right coordinates not necessarily where the mouse is
+.lefter {
+	padding: auto;
+	width: 25%;
+	float: left;
+	height: 100%;
+	border: solid;
+	border-color: #DBB36A;
+	position: static;
 }
 
-function draw() {
-	//draw the board at every frame
-	fill(242, 176, 109); //draws the frame of the board 
-	rect(0, 0, 540, 540);
-	board.drawBoard(); //draws the board itself
-	board.drawStones(); //draws any stones that need to be drawn on the board
-	board.drawStones();
-	//if (player.currentTurn) {
-		checkMousePosition(); //checks the mouse to get the coordinates of the nearest stone to be placed	
-	//}
+.righter {
+	margin: auto;
+	width: 25%;
+	float: right;
+	height: 100px;
+	border: solid;
+	border-color: #DBB36A;
+	position: static;
+	top: 50px;
+}
+
+.toper {
+	margin: auto;
+	width: 100%;
+	height: 30px;
+	position: static;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+
+}
+
+li {
+    float: left;
+    border-right:1px solid #bbb;
+}
+
+li:last-child {
+    border-right: none;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 20px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+    background-color: #4CAF50;
 }
